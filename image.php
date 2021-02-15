aee3f2ee2f04693943ca7e4c135b12601e146ebb
RZ <30381083+w311ang@users.noreply.github.com>
2021-02-15 22:15:34 +0800

    Update image.php

--- image.php
+++ image.php
@@ -1,53 +1,53 @@
 <?php
-        $stime=microtime(true);
-        if(file_exists("config.php")) {
-                require "config.php";
-        } else {
-                define("storage",dirname(__FILE__)."/storage/");
-                define("chatfile","chat.log");
-                define("linkchar1","&");
-                define("linkchar2","'says'");
-        };
-        if(!defined("storage")){
-                define("storage",dirname(__FILE__)."/storage/");
-        };
-        if(!defined("chatfile")){
-                define("chatfile","chat.log");
-        };
-        if(!defined("linkchar1")){
-                define("linkchar1","&");
-        };
-        if(!defined("linkchar2")){
-                define("linkchar2","'says'");
-        };
-        try{
-                date_default_timezone_set('PRC');
-                if (getenv('HTTP_CLIENT_IP')) {
-                        $ip = getenv('HTTP_CLIENT_IP');
-                } elseif (getenv('HTTP_X_FORWARDED_FOR')) {
-                        $ip = getenv('HTTP_X_FORWARDED_FOR');
-                } elseif (getenv('HTTP_X_FORWARDED')) {
-                        $ip = getenv('HTTP_X_FORWARDED');
-                } elseif (getenv('HTTP_FORWARDED_FOR')) {
-                        $ip = getenv('HTTP_FORWARDED_FOR');
-                } elseif (getenv('HTTP_FORWARDED')) {
-                        $ip = getenv('HTTP_FORWARDED');
-                } elseif (getenv('REMOTE_ADDR')) {
-                        $ip = getenv('REMOTE_ADDR');
-                } elseif (!empty($_SERVER['REMOTE_ADDR'])) {
-                        $ip = $_SERVER['REMOTE_ADDR'];
-                } else {
-                        $ip = "Unknown";
-                };
-                if (empty($_POST['username'])){
-                $etime=microtime(true);
-                $totaltime=$etime-$stime;
-                echo "User Name empty.</br>Total time: ".$totaltime."s.";
-                        exit;
-                };
+	$stime=microtime(true);
+	if(file_exists("config.php")) {
+		require "config.php";
+	} else {
+		define("storage",dirname(__FILE__)."/storage/");
+		define("chatfile","chat.log");
+		define("linkchar1","&");
+		define("linkchar2","'says'");
+	};
+	if(!defined("storage")){
+		define("storage",dirname(__FILE__)."/storage/");
+	};
+	if(!defined("chatfile")){
+		define("chatfile","chat.log");
+	};
+	if(!defined("linkchar1")){
+		define("linkchar1","&");
+	};
+	if(!defined("linkchar2")){
+		define("linkchar2","'says'");
+	};
+	try{
+		date_default_timezone_set('PRC');
+		if (getenv('HTTP_CLIENT_IP')) { 
+			$ip = getenv('HTTP_CLIENT_IP'); 
+		} elseif (getenv('HTTP_X_FORWARDED_FOR')) { 
+			$ip = getenv('HTTP_X_FORWARDED_FOR'); 
+		} elseif (getenv('HTTP_X_FORWARDED')) { 
+			$ip = getenv('HTTP_X_FORWARDED'); 
+		} elseif (getenv('HTTP_FORWARDED_FOR')) { 
+			$ip = getenv('HTTP_FORWARDED_FOR'); 
+		} elseif (getenv('HTTP_FORWARDED')) { 
+			$ip = getenv('HTTP_FORWARDED'); 
+		} elseif (getenv('REMOTE_ADDR')) { 
+			$ip = getenv('REMOTE_ADDR'); 
+		} elseif (!empty($_SERVER['REMOTE_ADDR'])) { 
+			$ip = $_SERVER['REMOTE_ADDR']; 
+		} else {
+			$ip = "Unknown"; 
+		};
+		if (empty($_POST['username'])){
+		$etime=microtime(true);
+		$totaltime=$etime-$stime;
+		echo "User Name empty.</br>Total time: ".$totaltime."s.";
+			exit;
+		};
 if($_FILES["file"]["error"])
 {
-    echo $_FILES["file"]["error"];
+    echo $_FILES["file"]["error"];    
 }
 else
 {
@@ -57,39 +57,39 @@ else
             $filename =iconv("gb2312","UTF-8",$filename);
             if(file_exists($filename))
             {
-                $etime=microtime(true);
-                $totaltime=$etime-$stime;
+		$etime=microtime(true);
+		$totaltime=$etime-$stime;
         echo"File exist.</br>Total time: ".$totaltime."s.";
-                exit();
+		exit();
             }
             else
-            {
+            {  
                 move_uploaded_file($_FILES["file"]["tmp_name"],$filename);
-            }
+            }        
     }
     else
     {
-                $etime=microtime(true);
-                $totaltime=$etime-$stime;
+		$etime=microtime(true);
+		$totaltime=$etime-$stime;
         echo"Unknown file type.</br>Total time: ".$totaltime."s.";
-                exit();
+		exit();
     }
 }
-                $un = $_POST['username'];
-                $time = date('H:i',time());
-                $timehidden = date('Y-m-d H:i:s',time());
-                if(file_exists(storage.''.chatfile)) {
-                        $myfile = fopen(storage.''.chatfile, "a") or die("Unable to open file!");
-                        fwrite($myfile,$ip . "+" . $timehidden . "" . linkchar1 . "[".$time."] [" . $un . "] " .linkchar2 . "" .  "<img src=\"".str_replace(dirname(__FILE__), '', ".".$filename)."\""." width=\"80%\""." />" . "\r\n");
-                        fclose($myfile);
-                } else {
-                        file_put_contents(storage.''.chatfile, $ip . "+" . $timehidden . "" . linkchar1 . "[".$time."] [" . $un . "] " .linkchar2 . "" . "<img src=\"".str_replace(dirname(__FILE__), '', ".".$filename)."\""." width=\"80%\""." />" . "\r\n");
-                };
-                $etime=microtime(true);
-                $totaltime=$etime-$stime;
-                echo "Success.</br>Total time: ".$totaltime."s.<script>window.opener=null;window.close();</script>";
+		$un = $_POST['username'];
+		$time = date('H:i',time());
+		$timehidden = date('Y-m-d H:i:s',time());
+		if(file_exists(storage.''.chatfile)) {
+			$myfile = fopen(storage.''.chatfile, "a") or die("Unable to open file!");
+			fwrite($myfile,$ip . "+" . $timehidden . "" . linkchar1 . "[".$time."] [" . $un . "] " .linkchar2 . "" .  "<img src=\"".str_replace(dirname(__FILE__), '', ".".$filename)."\" />" . "\r\n");
+			fclose($myfile);
+		} else {
+			file_put_contents(storage.''.chatfile, $ip . "+" . $timehidden . "" . linkchar1 . "[".$time."] [" . $un . "] " .linkchar2 . "" . "<img src=\"".str_replace(dirname(__FILE__), '', ".".$filename)."\" />" . "\r\n");
+		};
+		$etime=microtime(true);
+		$totaltime=$etime-$stime;
+		echo "Success.</br>Total time: ".$totaltime."s.<script>window.opener=null;window.close();</script>";
 
-        }catch(Exception $e){
-                echo $e->getMessage();
-                exit();
-        }
+	}catch(Exception $e){
+		echo $e->getMessage();
+		exit();
+	}
